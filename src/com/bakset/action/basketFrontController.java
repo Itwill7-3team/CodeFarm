2e4b71bb90863e57e7b04351a1da8120947de7e4
ByeonJaeJeong <qsz78547@naver.com>
2020-05-04 12:26:35 +0900

    src test

---
+++ src/com/bakset/action/basketFrontController.java
@@ -0,0 +1,72 @@
+package com.bakset.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class basketFrontController extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet호출");
+		doProcess(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost호출");
+		doProcess(req, resp);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("-----------[AdminGoodsFrontController]doProcess호출---------");
+		Action action = null;
+		ActionForward forward = null;
+		
+		String requestURI = request.getRequestURI();
+		System.out.println("URI : " + requestURI);
+
+		String contextPath = request.getContextPath();
+		System.out.println("ContextPath(프로젝트명) : " + contextPath);
+
+		String command = requestURI.substring(contextPath.length());
+		System.out.println("command : " + command);
+
+		System.out.println("----------페이지 주소 계산 완료----------------------");
+		
+		System.out.println("----------------------페이지구분(view/model)--------------------");
+		if(command.equals("/GoodsAdd.ag")){
+			
+			
+			
+		}
+		
+		
+		System.out.println("-----------페이지 이동(redirect(true)/forward(false))---------------");
+		// 페이지 이동정보가 있을때만 페이지 이동
+		
+		if (forward != null) {
+			if (forward.isRedirect()) {
+				// true
+				System.out.println("sendRedirect() 이동 : " + forward.getPath());
+				response.sendRedirect(forward.getPath());
+			} else {
+				// false
+				System.out.println("forward() 이동: " + forward.getPath());
+				RequestDispatcher dis = request.getRequestDispatcher(forward.getPath());
+				dis.forward(request, response);
+			}
+		}
+
+	
+	}
+}
