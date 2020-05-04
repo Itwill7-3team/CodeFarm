edf6b30e3828d0a023b76d04f0600a8b99294e96
JaeJeong <59276139+ByeonJaeJeong@users.noreply.github.com>
2020-05-04 22:52:08 +0900

    Add files via upload
    db ~

--- codefarm.sql
+++ codefarm.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `codefarm` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `codefarm`;
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
 --
 -- Host: localhost    Database: codefarm
 -- ------------------------------------------------------
--- Server version	5.6.45-log
+-- Server version	5.7.28-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +16,33 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `basket`
+--
+
+DROP TABLE IF EXISTS `basket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `basket` (
+  `b_num` int(11) NOT NULL AUTO_INCREMENT,
+  `b_u_id` varchar(45) NOT NULL,
+  `b_l_num` int(11) NOT NULL,
+  `b_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `b_l_price` int(11) NOT NULL DEFAULT '0',
+  `b_l_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`b_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `basket`
+--
+
+LOCK TABLES `basket` WRITE;
+/*!40000 ALTER TABLE `basket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `basket` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `board`
@@ -106,6 +135,38 @@ LOCK TABLES `lecture` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `orderlist`
+--
+
+DROP TABLE IF EXISTS `orderlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderlist` (
+  `o_num` int(11) NOT NULL AUTO_INCREMENT,
+  `o_b_num` int(11) NOT NULL,
+  `o_l_price` int(11) NOT NULL,
+  `o_l_num` int(11) NOT NULL,
+  `o_l_name` varchar(45) NOT NULL,
+  `o_u_id` varchar(45) NOT NULL,
+  `o_t_type` varchar(45) NOT NULL,
+  `o_t_bank` varchar(45) NOT NULL,
+  `o_t_payer` varchar(45) NOT NULL,
+  `o_t_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `o_sum_money` int(11) NOT NULL,
+  PRIMARY KEY (`o_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orderlist`
+--
+
+LOCK TABLES `orderlist` WRITE;
+/*!40000 ALTER TABLE `orderlist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orderlist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `star`
 --
 
@@ -130,6 +191,32 @@ CREATE TABLE `star` (
 LOCK TABLES `star` WRITE;
 /*!40000 ALTER TABLE `star` DISABLE KEYS */;
 /*!40000 ALTER TABLE `star` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `u_orderlist`
+--
+
+DROP TABLE IF EXISTS `u_orderlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `u_orderlist` (
+  `u_o_num` int(11) NOT NULL,
+  `u_u_num` int(11) DEFAULT NULL,
+  `u_l_num` int(11) DEFAULT NULL,
+  `u_l_name` varchar(45) DEFAULT NULL,
+  `u_l_img` text,
+  PRIMARY KEY (`u_o_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `u_orderlist`
+--
+
+LOCK TABLES `u_orderlist` WRITE;
+/*!40000 ALTER TABLE `u_orderlist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `u_orderlist` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -161,8 +248,29 @@ LOCK TABLES `user` WRITE;
 UNLOCK TABLES;
 
 --
--- Dumping events for database 'codefarm'
+-- Table structure for table `wishlist`
 --
+
+DROP TABLE IF EXISTS `wishlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `wishlist` (
+  `w_num` int(11) NOT NULL AUTO_INCREMENT,
+  `w_u_id` varchar(45) NOT NULL,
+  `w_l_num` int(11) NOT NULL,
+  `w_date` varchar(45) NOT NULL,
+  PRIMARY KEY (`w_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wishlist`
+--
+
+LOCK TABLES `wishlist` WRITE;
+/*!40000 ALTER TABLE `wishlist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wishlist` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Dumping routines for database 'codefarm'
@@ -177,4 +285,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-04 17:44:32
+-- Dump completed on 2020-05-04 22:51:20
