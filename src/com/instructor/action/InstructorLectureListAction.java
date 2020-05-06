fb4a71ca1947e13c58bf39440ed4d8e968d6ce75
MSI <MSI@DESKTOP-7SC776F>
2020-05-06 20:59:34 +0900

    Test

---
+++ src/com/instructor/action/InstructorLectureListAction.java
@@ -0,0 +1,19 @@
+package com.instructor.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class InstructorLectureListAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		System.out.println("GoodsListAction-execute()");
+		
+		
+		
+		
+		return null;
+	}
+
+}
