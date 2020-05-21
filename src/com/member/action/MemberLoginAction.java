11e2ee8eb586758325ae28f58c5da65b2c0ac7d4
ByeonJaeJeong <qsz78547@naver.com>
2020-05-21 10:28:49 +0900

    update nick

--- src/com/member/action/MemberLoginAction.java
+++ src/com/member/action/MemberLoginAction.java
@@ -95,7 +95,7 @@ public class MemberLoginAction implements Action {
 		session.setAttribute("m_email", m_email);
 		
 		MemberDTO mdto =new MemberDAO().getInfo(m_email);
-		String nick = mdto.getM_name();
+		String nick = mdto.getM_nick();
 		session.setAttribute("nick", nick);
 		
 		// 페이지 이동(메인페이지)
